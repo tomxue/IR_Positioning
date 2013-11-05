74e10e8152db52d8455128ce007bdc18b37a50ce
Tom Xue <tomxue@outlook.com>
2013-11-05 15:07:44 +0800

    optimised

--- StructureLightGenerator/StructureLightGenerator/MainWindow.xaml.cs
+++ StructureLightGenerator/StructureLightGenerator/MainWindow.xaml.cs
@@ -10,11 +10,15 @@ namespace StructureLightGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
-        byte[] randomValues = new byte[1280];
-        byte[] patternValue = new byte[1280];
-        byte[] match111 = new byte[1280];
-        byte[] match000 = new byte[1280];
-        byte[] match111Or000 = new byte[1280];
+        const int windowSize = 25;
+        const int consecutiveBits = 3;
+        const int resolutionX = 1280;
+        const int resolutionY = 800;
+        byte[] randomValues = new byte[resolutionX];
+        byte[] patternValue = new byte[resolutionX];
+        byte[] match111 = new byte[resolutionX];
+        byte[] match000 = new byte[resolutionX];
+        byte[] match111Or000 = new byte[resolutionX];
 
         public MainWindow()
         {
@@ -25,23 +29,23 @@ namespace StructureLightGenerator
             InitializeComponent();
 
             Random random = new Random();
-            ShowRandomNumbers(random);  // generate all the 1280 random numbers at this point
+            ShowRandomNumbers(random);  // generate all the resolutionX random numbers at this point
 
-            Bitmap bitmap = new Bitmap(1280, 800);  // Coolux DLP projector's resolution
+            Bitmap bitmap = new Bitmap(resolutionX, resolutionY);  // Coolux DLP projector's resolution
             Graphics g = Graphics.FromImage(bitmap);
             g.Clear(Color.Black);
 
-            for (int i = 0; i < 1280; i++)
+            for (int i = 0; i < resolutionX; i++)
             {
                 if (randomValues[i] % 2 == 1)
                 {
                     CountOf111++;
                     CountOf000 = 0;
                     // Requirement 1: limit the maximum number of con-secutive identical bits (a run of bits) to three
-                    if (CountOf111 == 3)
+                    if (CountOf111 == consecutiveBits)
                     {
                         CountOf111 = 0;
-                        g.DrawLine(new Pen(Color.Black), i, 0, i, 1280);
+                        g.DrawLine(new Pen(Color.Black), i, 0, i, resolutionX);
                         patternValue[i] = 0;
 
                         match111[i] = 1;
@@ -50,7 +54,7 @@ namespace StructureLightGenerator
                     }
                     else
                     {
-                        g.DrawLine(new Pen(Color.White), i, 0, i, 1280);
+                        g.DrawLine(new Pen(Color.White), i, 0, i, resolutionX);
                         patternValue[i] = 1;
                     }
                 }
@@ -59,10 +63,10 @@ namespace StructureLightGenerator
                     CountOf111 = 0;
                     CountOf000++;
                     // Requirement 1: limit the maximum number of con-secutive identical bits (a run of bits) to three
-                    if (CountOf000 == 3)
+                    if (CountOf000 == consecutiveBits)
                     {
                         CountOf000 = 0;
-                        g.DrawLine(new Pen(Color.White), i, 0, i, 1280);
+                        g.DrawLine(new Pen(Color.White), i, 0, i, resolutionX);
                         patternValue[i] = 1;
 
                         match000[i] = 1;
@@ -71,52 +75,54 @@ namespace StructureLightGenerator
                     }
                     else
                     {
-                        g.DrawLine(new Pen(Color.Black), i, 0, i, 1280);
+                        g.DrawLine(new Pen(Color.Black), i, 0, i, resolutionX);
                         patternValue[i] = 0;
                     }
                 }
 
                 // Requirement2: every window contain at least one run of length exactly one
                 CountAftermatch111or000++;
-                if (CountAftermatch111or000 == 21)
+                if (CountAftermatch111or000 == (windowSize - consecutiveBits - 1))
                 {
                     if (patternValue[i] == 1)
                     {
-                        randomValues[i + 1] = 0;
-                        randomValues[i + 2] = 0;
-                        randomValues[i + 3] = 0;
-                        randomValues[i + 4] = 1;
+                        int j;
+                        for (j = 1; j <= consecutiveBits; j++)
+                            randomValues[i + j] = 0;
+                        randomValues[i + j] = 1;
                     }
                     else
                     {
-                        randomValues[i + 1] = 1;
-                        randomValues[i + 2] = 1;
-                        randomValues[i + 3] = 1;
-                        randomValues[i + 4] = 0;
+                        int j;
+                        for (j = 1; j <= consecutiveBits; j++)
+                            randomValues[i + j] = 1;
+                        randomValues[i + j] = 0;
                     }
                 }
             }
 
             // show it
-            Console.WriteLine("Show pattern below:");
-            for (int i = 0; i < 1280; i++)
+            Console.WriteLine("\r\nShow pattern below:");
+            for (int i = 0; i < resolutionX; i++)
                 Console.Write("{0, 5}", patternValue[i]);
 
             Console.WriteLine("\r\nShow match111 below:");
-            for (int i = 0; i < 1280; i++)
+            for (int i = 0; i < resolutionX; i++)
                 Console.Write("{0, 5}", match111[i]);
 
             Console.WriteLine("\r\nShow match000 below:");
-            for (int i = 0; i < 1280; i++)
+            for (int i = 0; i < resolutionX; i++)
                 Console.Write("{0, 5}", match000[i]);
 
             Console.WriteLine("\r\nShow match111Or000 below:");
-            for (int i = 0; i < 1280; i++)
+            for (int i = 0; i < resolutionX; i++)
                 Console.Write("{0, 5}", match111Or000[i]);
 
             Console.WriteLine("\r\nShow modified random below:");
-            for (int i = 0; i < 1280; i++)
+            for (int i = 0; i < resolutionX; i++)
                 Console.Write("{0, 5}", randomValues[i]);
+
+            Console.WriteLine("\r\n");
 
             g.Save();
             g.Dispose();
