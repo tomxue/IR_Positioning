876c1d986ec74f8ae1b8b8082d3e2e2eea1e8974
Tom Xue <tomxue0126@gmail.com>
2013-12-25 17:04:32 +0800

    maple mini can read 2 ADCs under Dual regular simultaneous mode

--- IR_Positioning_pde/main.cpp
+++ IR_Positioning_pde/main.cpp
@@ -1,6 +1,8 @@
 // pin 19 (Yellow): CLK
 // pin 20 (Orange): SI of x axis
 // pin 18 (Blue)  : SI of y axis
+// pin 3 (Brown)  : AO of x axis
+// pin 4 (Green)  : AO of y axis
 // Red            : 5V
 // White          : Gnd
 
@@ -12,14 +14,20 @@
 unsigned long start = 0;
 unsigned long stop = 0;
 unsigned long pixelIndex = 0;
-unsigned long sampleCount = 100;
-unsigned int pixelVal[129];
+uint32 pixelVal_x[129];
+uint32 pixelVal_y[129];
+unsigned long sum_x = 0;
+unsigned long sum_y = 0;
+double threshold_x = 0;
+double threshold_y = 0;
+unsigned int x32_1, x32_2, x32_3, x32_4;
+unsigned int y32_1, y32_2, y32_3, y32_4;
 
 int SI_x = 20;
 int SI_y = 18;
 int CLK = 19;
-int AO_x = 3;
-int AO_y = 4;
+int AO_x = 3;   // channel 8
+int AO_y = 4;   // channel 7
 
 // GPIO
 gpio_dev *CLK_dev;
@@ -29,32 +37,26 @@ uint8 SI_x_pin;
 gpio_dev *SI_y_dev;
 uint8 SI_y_pin;
 
-// ADC
-const adc_dev *x_dev;
-adc_reg_map *x_regs;
-const adc_dev *y_dev;
-adc_reg_map *y_regs;
-
-uint32 adc_sequence = 0; //Temporary
+uint32 adc_sequence = 0;
 uint8 adc_length = 1; //The number of channels to be converted per ADC channel
 uint8 ADC1_Sequence[] = { /*The ADC1 channels sequence, left to right*/
-  8 }; /* Set the sequence 1-6 for SQR3 (left will be first). Must top up to all 6 channels with zeros */
+  8 }; /* channel 8 - pin 4. Set the sequence 1-6 for SQR3 (left will be first). Must top up to all 6 channels with zeros */
 uint8 ADC2_Sequence[] = { /*The ADC2 channels sequence, left to right*/
-  7 };
+  7 };  /* channel 7 - pin 3 */
 
 /*
 * calc_adc_sequence(ADCx_Sequence) converts the SQR3 6 channels' (each ADC1 and ADC2) list into
-* a valid 6 X 5=30 bits sequence format and returns that 30 bits number.
-* For more channels, repeat the same for SQR2, SQR1. (For SQR1 4 channels only!)
-*/
+ * a valid 6 X 5=30 bits sequence format and returns that 30 bits number.
+ * For more channels, repeat the same for SQR2, SQR1. (For SQR1 4 channels only!)
+ */
 uint32 calc_adc_sequence(uint8 adc_sequence_array[6])
 {
   adc_sequence = 0;
   for (int i = 0; i<6; i++) // There are 6 available sequences in each SQR3 SQR2, and 4 in SQR1.
   {
     /*This function converts the array into one number by multiplying each 5-bits channel numbers
-by multiplications of 2^5
-*/
+     by multiplications of 2^5
+     */
     adc_sequence = adc_sequence + adc_sequence_array[i] * pow(2, (i * 5));
   }
   return adc_sequence;
@@ -62,138 +64,257 @@ by multiplications of 2^5
 
 void setup()
 {
-    // the following line is needed for Maple
-    pinMode(AO_x, INPUT_ANALOG);
-    pinMode(AO_y, INPUT_ANALOG);
-    pinMode(CLK, OUTPUT);
-    pinMode(SI_x, OUTPUT);
-    pinMode(SI_y, OUTPUT);
-    // 下面的代码可能不全，会导致串口无输出
-    adc_init(ADC1); //rcc_clk_enable(ADC1->clk_id), Must be the first adc command!
-    adc_init(ADC2);
+  // the following line is needed for Maple
+  pinMode(AO_x, INPUT_ANALOG);
+  pinMode(AO_y, INPUT_ANALOG);
+  pinMode(CLK, OUTPUT);
+  pinMode(SI_x, OUTPUT);
+  pinMode(SI_y, OUTPUT);
 
-    adc_enable(ADC1); //ADC_CR2_ADON_BIT = 1
-    adc_enable(ADC2);
+  adc_init(ADC1); //rcc_clk_enable(ADC1->clk_id), Must be the first adc command!
+  adc_init(ADC2);
 
-    adc_calibrate(ADC1); //Optional
-    adc_calibrate(ADC2);
+  adc_enable(ADC1); //ADC_CR2_ADON_BIT = 1
+  adc_enable(ADC2);
 
-    // 采样率优化的关键寄存器参数
-    adc_set_prescaler(ADC_PRE_PCLK2_DIV_2);
-    adc_set_sample_rate(ADC1, ADC_SMPR_1_5);
-    adc_set_sample_rate(ADC2, ADC_SMPR_1_5);
+  adc_calibrate(ADC1); //Optional
+  adc_calibrate(ADC2);
 
-    adc_set_exttrig(ADC1, 1); //External trigger must be Enabled for both ADC.
-    adc_set_exttrig(ADC2, 1); //External trigger must be Enabled for both ADC.
-    adc_set_extsel(ADC1, ADC_ADC12_SWSTART); //External trigger Event
-    adc_set_extsel(ADC2, ADC_ADC12_SWSTART); //External trigger Event
+  // 采样率优化的关键寄存器参数
+  adc_set_prescaler(ADC_PRE_PCLK2_DIV_2);
+  adc_set_sample_rate(ADC1, ADC_SMPR_1_5);
+  adc_set_sample_rate(ADC2, ADC_SMPR_1_5);
 
-    adc_set_reg_seqlen(ADC1, adc_length); //The number of channels to be converted.
-    adc_set_reg_seqlen(ADC2, adc_length);
-    ADC1->regs->SQR3 |= calc_adc_sequence(ADC1_Sequence);
-    ADC2->regs->SQR3 |= calc_adc_sequence(ADC2_Sequence);  
+  adc_set_exttrig(ADC1, 1); //External trigger must be Enabled for both ADC.
+  adc_set_exttrig(ADC2, 1); //External trigger must be Enabled for both ADC.
+  adc_set_extsel(ADC1, ADC_ADC12_SWSTART); //External trigger Event
+  adc_set_extsel(ADC2, ADC_ADC12_SWSTART); //External trigger Event
 
-    ADC1->regs->CR1 &= ~(16 << 16);     // 0000: independent mode. 
-    ADC2->regs->CR1 &= ~(16 << 16);     // 0000: independent mode. 
+  adc_set_reg_seqlen(ADC1, adc_length); //The number of channels to be converted.
+  adc_set_reg_seqlen(ADC2, adc_length);
+  ADC1->regs->SQR3 |= calc_adc_sequence(ADC1_Sequence);
+  ADC2->regs->SQR3 |= calc_adc_sequence(ADC2_Sequence);  
 
-    // digitalWrite() preparation work
-    // CLK
-    CLK_dev = PIN_MAP[CLK].gpio_device;
-    CLK_pin = PIN_MAP[CLK].gpio_bit;
-    // SI x
-    SI_x_dev = PIN_MAP[SI_x].gpio_device;
-    SI_x_pin = PIN_MAP[SI_x].gpio_bit;
-    // SI y
-    SI_y_dev = PIN_MAP[SI_y].gpio_device;
-    SI_y_pin = PIN_MAP[SI_y].gpio_bit;
+  // right alligned
+  ADC1->regs->CR2 &= (0xfffff7ff);
+  ADC2->regs->CR2 &= (0xfffff7ff);
 
-    // analogRead() preparation work
-    // X analog in
-    x_dev = PIN_MAP[AO_x].adc_device;
-    x_regs = x_dev->regs;
-    adc_set_reg_seqlen(x_dev, 1);
-//    x_regs->SQR3 = PIN_MAP[AO_x].adc_channel;
-    // Y analog in
-    y_dev = PIN_MAP[AO_y].adc_device;
-    y_regs = y_dev->regs;
-    adc_set_reg_seqlen(y_dev, 1);
-//    y_regs->SQR3 = PIN_MAP[AO_y].adc_channel;
+  // DMA bit set, the key!!! I was stucked here for several days.
+  // RM0008 page 219: Note: In dual ADC mode, to read the slave converted data on the master data register, the DMA 
+  // bit must be enabled even if it is not used to transfer converted regular channel data.
+  ADC1->regs->CR2 |= (1<<8);
+  ADC2->regs->CR2 |= (1<<8);
+
+  ADC1->regs->CR1 |= (6 << 16);       // 0b0110: Regular simultaneous mode only
+  //  ADC2->regs->CR1 |= (6 << 16);     // For CR1 DUALMOD, These bits are reserved in ADC2 and ADC3. RM0008 page 229
+
+  // digitalWrite() preparation work
+  // CLK
+  CLK_dev = PIN_MAP[CLK].gpio_device;
+  CLK_pin = PIN_MAP[CLK].gpio_bit;
+  // SI x
+  SI_x_dev = PIN_MAP[SI_x].gpio_device;
+  SI_x_pin = PIN_MAP[SI_x].gpio_bit;
+  // SI y
+  SI_y_dev = PIN_MAP[SI_y].gpio_device;
+  SI_y_pin = PIN_MAP[SI_y].gpio_bit;
+}
+
+void sampleSensor()
+{
+  for(int i = 0; i<2; i++)
+  {
+    pixelIndex = 0;
+
+    // digitalWrite(CLK, LOW);
+    CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;    // CLK set low
+    SI_x_dev->regs->BSRR = (1U << SI_x_pin)<<16;  // SI x set low
+    SI_y_dev->regs->BSRR = (1U << SI_y_pin)<<16;  // SI y set low
+    SI_x_dev->regs->BSRR = (1U << SI_x_pin);      // SI x set high
+    SI_y_dev->regs->BSRR = (1U << SI_y_pin);      // SI y set high
+    CLK_dev->regs->BSRR = (1U << CLK_pin);        // CLK set high
+
+    // start the 1st ADC sample and conversion
+    ADC1->regs->CR2 |= ADC_CR2_SWSTART;
+    while (!(ADC1->regs->SR & ADC_SR_EOC))
+      ;
+    pixelVal_x[pixelIndex] = (uint16)(ADC1->regs->DR);                            // low 16 bits
+    pixelVal_y[pixelIndex] = ((uint32)(ADC1->regs->DR & ADC_DR_ADC2DATA))>>16;    // high 16 bits
+
+    SI_x_dev->regs->BSRR = (1U << SI_x_pin)<<16;  // SI x set low
+    SI_y_dev->regs->BSRR = (1U << SI_y_pin)<<16;  // SI y set low
+    CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;    // CLK set low
+
+    while (pixelIndex < 128)
+    {
+      pixelIndex++;
+
+      // digitalWrite(CLK, HIGH);
+      CLK_dev->regs->BSRR = (1U << CLK_pin);
+
+      // pixelVal_x[pixelIndex] = analogRead(AO_x);
+      ADC1->regs->CR2 |= ADC_CR2_SWSTART;
+      while (!(ADC1->regs->SR & ADC_SR_EOC))
+        ;
+      pixelVal_x[pixelIndex] = (uint16)(ADC1->regs->DR);                            // low 16 bits
+      pixelVal_y[pixelIndex] = ((uint32)(ADC1->regs->DR & ADC_DR_ADC2DATA))>>16;    // high 16 bits
+      // the last one: pixelIndex = 128
+
+      // digitalWrite(CLK, LOW);
+      CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;
+    }
+  }
+}
+
+void calcThreshold()
+{
+  sum_x = 0;
+  sum_y = 0;
+  threshold_x = 0;
+  threshold_y = 0;
+
+  for(int i=0;i<128;i++)
+  {
+    sum_x += pixelVal_x[i];
+    sum_y += pixelVal_y[i];
+  }
+  threshold_x = sum_x / 128.0;
+  threshold_y = sum_y / 128.0;
+}
+
+void digitize()
+{
+  x32_1 = 0;
+  x32_2 = 0;
+  x32_3 = 0;
+  x32_4 = 0;
+  y32_1 = 0;
+  y32_2 = 0;
+  y32_3 = 0;
+  y32_4 = 0;
+
+  for(int i=0;i<32;i++)
+  {
+    if(pixelVal_x[i] >= threshold_x)
+      x32_1 |= 1 << (31-i);
+    else
+      x32_1 &= ~(1 << (31-i));
+
+    if(pixelVal_x[i+32] >= threshold_x)
+      x32_2 |= 1 << (31-i);
+    else
+      x32_2 &= ~(1 << (31-i));
+
+    if(pixelVal_x[i+64] >= threshold_x)
+      x32_3 |= 1 << (31-i);
+    else
+      x32_3 &= ~(1 << (31-i));
+
+    if(pixelVal_x[i+96] >= threshold_x)
+      x32_4 |= 1 << (31-i);
+    else
+      x32_4 &= ~(1 << (31-i));
+
+    if(pixelVal_y[i] >= threshold_y)
+      y32_1 |= 1 << (31-i);
+    else
+      y32_1 &= ~(1 << (31-i));
+
+    if(pixelVal_y[i+32] >= threshold_y)
+      y32_2 |= 1 << (31-i);
+    else
+      y32_2 &= ~(1 << (31-i));
+
+    if(pixelVal_y[i+64] >= threshold_y)
+      y32_3 |= 1 << (31-i);
+    else
+      y32_3 &= ~(1 << (31-i));
+
+    if(pixelVal_y[i+96] >= threshold_y)
+      y32_4 |= 1 << (31-i);
+    else
+      y32_4 &= ~(1 << (31-i));
+  }
 }
 
 void loop()
 {
-    COM.println("\nStarting loops:");
-    
-    start = micros();
-    for(int m=0;m<sampleCount;m++)
-    {
-        pixelIndex = 0;
-        
-        // digitalWrite(CLK, LOW);
-        CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;    // CLK set low
-        SI_x_dev->regs->BSRR = (1U << SI_x_pin)<<16;  // SI x set low
-        SI_x_dev->regs->BSRR = (1U << SI_x_pin);      // SI x set high
-        CLK_dev->regs->BSRR = (1U << CLK_pin);        // CLK set high
-        x_regs->CR2 |= ADC_CR2_SWSTART;
-        while (!(x_regs->SR & ADC_SR_EOC))
-            ;
-        pixelVal[pixelIndex] = (uint16)(x_regs->DR & ADC_DR_DATA);
-        SI_x_dev->regs->BSRR = (1U << SI_x_pin)<<16;  // SI x set low
-        CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;    // CLK set low
-        
-        while (pixelIndex < 128)
-        {
-            pixelIndex++;
+  COM.println("\nStarting loops:");
 
-            // digitalWrite(CLK, HIGH);
-            CLK_dev->regs->BSRR = (1U << CLK_pin);
+  start = micros();
+  sampleSensor();
+  calcThreshold();
+  digitize();
+  stop = micros();
 
-//          pixelVal[pixelIndex] = analogRead(AO_x);
-            x_regs->CR2 |= ADC_CR2_SWSTART;
-            while (!(x_regs->SR & ADC_SR_EOC))
-                ;
-            pixelVal[pixelIndex] = (uint16)(x_regs->DR & ADC_DR_DATA);
+  COM.println("Stop loops:");
+  COM.print("Elapsed Time: ");
+  COM.print(stop - start);
+  COM.print(" us (for ");
+  COM.print(1*129*2);
+  COM.println(" analog reads)");
+  COM.print((stop-start)/(double)(1*129*2));
+  COM.print(" us (for 1 sample) ");
+  COM.print((stop-start)/(double)(1*2));
+  COM.print(" us (for 1 sensor: 129 pixels) ");
 
-            // digitalWrite(CLK, LOW);
-            CLK_dev->regs->BSRR = (1U << CLK_pin)<<16;
-        }
-    }
-    stop = micros();
+  COM.println(" pixelVal_x = ");
+  for(int k=0;k<128;k++)
+  {
+    COM.print(pixelVal_x[k]);
+    COM.print("  ");
+    if(k % 20 == 0)
+      COM.println("");
+  }
 
-    COM.println("Stop loops:");
-    COM.print("Elapsed Time: ");
-    COM.print(stop - start);
-    COM.print(" us (for ");
-    COM.print(sampleCount*128);
-    COM.println(" analog reads)");
-    COM.print((stop-start)/(double)(sampleCount*128));
-    COM.print(" us (for 1 sample) ");
-    COM.print((stop-start)/(double)(sampleCount));
-    COM.print(" us (for 1 sensor) ");
-    COM.println(" pixelVal = ");
-    for(int k=0;k<128;k++)
-    {
-        COM.print(pixelVal[k]);
-        COM.print("  ");
-        if(k % 20 == 0)
-            COM.println("");
-    }
-    COM.println(" PCLK2 = ");
-    COM.println(PCLK2);
+  COM.println("\n pixelVal_y = ");
+  for(int k=0;k<128;k++)
+  {
+    COM.print(pixelVal_y[k]);
+    COM.print("  ");
+    if(k % 20 == 0)
+      COM.println("");
+  }
+
+  COM.print("\n PCLK2 = ");
+  COM.println(PCLK2);
+  COM.print("\n ADC1->regs->CR1 = ");
+  COM.println(ADC1->regs->CR1, HEX);
+  COM.print("\n ADC2->regs->CR1 = ");
+  COM.println(ADC2->regs->CR1, HEX);
+  COM.print("\n threshold x is ");
+  COM.println(threshold_x);
+  COM.print("\n threshold y is ");
+  COM.println(threshold_y);
+  COM.print("x32_1 is: ");
+  COM.println(x32_1, HEX);
+  COM.print("x32_2 is: ");
+  COM.println(x32_2, HEX);
+  COM.print("x32_3 is: ");
+  COM.println(x32_3, HEX);
+  COM.print("x32_4 is: ");
+  COM.println(x32_4, HEX);
+  COM.print("y32_1 is: ");
+  COM.println(y32_1, HEX);
+  COM.print("y32_2 is: ");
+  COM.println(y32_2, HEX);
+  COM.print("y32_3 is: ");
+  COM.println(y32_3, HEX);
+  COM.print("y32_4 is: ");
+  COM.println(y32_4, HEX);
 }
 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated objects that need libmaple may fail.
 // 下面的代码缺失的话，下载后maple mini串口设备消失，无法调试
 __attribute__((constructor)) void premain() {
-    init();
+  init();
 }
 
 int main(void) {
-    setup();
-    while (true) {
-        loop();
-    }
-    return 0;
+  setup();
+  while (true) {
+    loop();
+  }
+  return 0;
 }
-
